d46c527231965f724d5272b889ae0a9075607e3b
sueanyv <valenciasue@hotmail.com>
2016-11-08 12:28:47 -0800

    more stuff

--- prac.js
+++ prac.js
@@ -1,10 +1,24 @@
 'use strict';
+// ****DO WHILE
+// var count = 0;
+// var name = 'sugey';
+// var num = 1;
+// do { document.write(num + ' ');
+//   num += 1;
+//   count += 1;
+// } while (count < 16);
+// document.write(name);
 
-var count = 0;
-var name = 'sugey';
-var num = 1;
-do { document.write(num + ' ');
-  num += 1;
-  count += 1;
-} while (count < 16);
-document.write(name);
+//***** WHILE
+// var counter = 0;
+// while (counter < 11) {
+//   document.write(counter + ' ');
+//   counter += 1;
+// }
+
+// ****FOR LOOP
+// var counter = 0;
+// document.write('Count Down ')
+// for (var counter = 0; counter < 11; counter += 1) {
+//   document.write( counter + ' ');
+// }
