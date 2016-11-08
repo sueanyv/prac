bad52050555ec94befecd9788860999b9660828f
sueanyv <valenciasue@hotmail.com>
2016-11-08 13:13:43 -0800

    more and more stuff

--- prac.js
+++ prac.js
@@ -18,7 +18,20 @@
 
 // ****FOR LOOP
 // var counter = 0;
-// document.write('Count Down ')
+// document.write('Count Down ');
 // for (var counter = 0; counter < 11; counter += 1) {
-//   document.write( counter + ' ');
+//   document.write(counter + ' ');
 // }
+
+// var html = '';
+// for (var counter = 0; counter <= 16; counter += 1) {
+//   html += '<div>' + counter + '</div>';
+// }
+// document.write(html);
+
+
+// var family = ['sugey ', ' wayne ', ' michael ', ' belkis ', ' wayne sr ', ' lillia ', ' carlos ', ' prince '];
+//
+// for (var i = 0; i > family.length; i++) {
+// }
+// document.write(' <div> ' + family + ' </div> ');
